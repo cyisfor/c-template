5732632c692b26fe9c055e5364b612891d6cd855
user <nobody>
2016-09-01 08:23:00 +0000

    auto (db.c) 93 857

--- apply.c
+++ apply.c
@@ -6,6 +6,11 @@
 #include <string.h> // memmem etc
 #include <unistd.h> // close
 
+#include <error.h>
+#include <errno.h>
+
+
+
 void assertp(const char* file, int line, const char* code, int test) {
 	if(!test) {
 		error(23,errno,"Assert fail %s:%d (%s)",file,line,code);
@@ -14,14 +19,14 @@ void assertp(const char* file, int line, const char* code, int test) {
 
 #define assert(test) assertp(__FILE__,__LINE__,#test,(int)(test))
 
-#define PUT(str, len) fwrite(str,1,len,stdout)
+#define PUT(str, len) fwrite(str,1,len,stdout); fflush(stdout);
 #define PUTLIT(lit) PUT(lit,sizeof(lit)-1)
 
 int main(int argc, char *argv[])
 {
 	struct stat info;
 	// must redirect a file from stdin to here.
-	assert(0==stat(0, &info));
+	assert(0==fstat(0, &info));
 	char* buf = mmap(NULL, info.st_size, PROT_READ, MAP_SHARED, 0, 0);
 	assert(buf != MAP_FAILED);
 	close(0);
@@ -34,8 +39,8 @@ int main(int argc, char *argv[])
 			break;
 		}
 		// output up to before the dollar
-		PUT(buf+offset,start-buf);
-		offset += start-buf+2;
+		PUT(buf+offset,start-(buf+offset));
+		offset = start-buf+2;
 		char* end = memmem(buf+offset,info.st_size-offset,")",1);
 		if(end == NULL) {
 			// output the dollar and stuff, to the end, no more piddies
@@ -44,21 +49,22 @@ int main(int argc, char *argv[])
 			break;
 		}
 		{
-			ssize_t amt = end-start;
+			ssize_t amt = end-start-2;
 			char* name = alloca(amt+1);
-			memcpy(name,start,amt);
+			memcpy(name,start+2,amt);
 			name[amt] = '\0';
 
 			const char* value = getenv(name);
 			if(NULL==value) {
+				// don't forget the $(
 				// output to the )
-				PUT(buf+offset,end-buf+1);
+				PUT(buf+offset-2,end-start+1);
 			} else {
 				PUT(value,strlen(value));
 			}
 		}
 		// past the )
-		offset += end-buf+1;
+		offset = end-buf+1;
 	}
 	return 0;
 }
