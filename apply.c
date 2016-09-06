ec89ea354c648b3fe3c88a7c358ec6cdf7d274ae
user <nobody>
2016-09-06 07:30:01 +0000

    auto (array.template.h) 55 641

--- apply.c
+++ apply.c
@@ -27,13 +27,13 @@ void apply_template(int dest, int source
 #endif
 										) {
 #ifndef DO_MAIN
-	string_array args;
+	string_array args = {};
 	va_list_to_string_arrayv(&args,varg);
 #endif
 	struct stat info;
 	// must redirect a file from stdin to here.
 	assert(0==fstat(source, &info));
-	char* buf = mmap(NULL, info.st_size, PROT_READ, MAP_SHARED, 0, 0);
+	char* buf = mmap(NULL, info.st_size, PROT_READ, MAP_SHARED, source, 0);
 	assert(buf != MAP_FAILED);
 	close(source);
 	off_t offset = 0;
@@ -89,9 +89,12 @@ void apply_template(int dest, int source
 
 #ifdef DO_MAIN
 // ugh... bootstrapping
-
-int main(void) {
-	apply_template(1,0);
+#include <fcntl.h>
+int main(int argc, char** argv) {
+	assert(argc == 2);
+	int source = open(argv[1],O_RDONLY);
+	assert(source >= 0);
+	apply_template(1,source);
 
 	return 0;
 }
